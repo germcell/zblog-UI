30a5ed3104a1315cebd7535a6b204c6774d0b5cc
germcell <441497343@qq.com>
2022-09-11 20:58:55 +0800

    获取到对话数据，准备显示

--- blog_static/js/my/message.js
+++ blog_static/js/my/message.js
@@ -1,12 +1,13 @@
 const messageVue = new Vue({
   el: "#message-container",
   data: {
-    sendContent: "",
-    userInfo: {},
-    privateMsgUserInfo: [],
-    unreadMsgNum: [],
-    currentUserInfo: {},
-    currentPrivateMsgInfo: [],
+    sendContent: "",  // 发送内容
+    userInfo: {}, // 登录信息
+    privateMsgUserInfo: [], // 私信用户信息
+    unreadMsgNum: [], // 未读私信数量
+    currentUserInfo: {}, // 当前选中的私信用户
+    currentReceiveMsg: [], // 之前接收的私信
+    currentSendMsg: [], // 之前回复的私信
   },
   async created() {
     this.userInfo = getLoginUserMsg()
@@ -49,24 +50,28 @@ const messageVue = new Vue({
     // 选择当前用户
     async selectUser(sendId) {
       this.currentUserInfo = this.privateMsgUserInfo.find(ele => {
+        ele.unread = 0
         return ele.sendId == sendId
       })
 
-    // 请求私信内容
-    const {data: res} = await axios({
-      url: baseUrl + '/v2/msg/private',
-      method: 'post',
-      data: JSON.stringify(this.currentUserInfo.commentIds),
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
+      // 请求私信内容
+      const {data: res} = await axios({
+        url: baseUrl + '/v2/msg/private',
+        method: 'post',
+        data: JSON.stringify(this.currentUserInfo.commentIds),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
 
-    if (res.code != 200) {
-      this.$message.error('获取私信失败')
-      return 
-    }
-    this.currentPrivateMsgInfo = res.data
+      if (res.code != 200) {
+        this.$message.error('获取私信失败')
+        return 
+      }
+      // this.currentReceiveMsg = res.data
+      
+     // TODO 消息按时间排列 
+      console.log(res.data);
 
     },
     logout() {
